4cc8620a6a444be9f977bbee9c65b72cb98bda27
Sachin <sachineducational555@gmail.com>
2024-08-25 10:48:51 +0530

    Updated: trending Albums

--- src/utils/fetchContent.js
+++ src/utils/fetchContent.js
@@ -1,18 +1,19 @@
 const BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? process.env.NEXT_PUBLIC_PRODUCTION_URL
-    : process.env.NEXT_PUBLIC_LOCAL_URL;
+    process.env.NODE_ENV === 'production'
+        ? process.env.NEXT_PUBLIC_PRODUCTION_URL
+        : process.env.NEXT_PUBLIC_LOCAL_URL;
 
 export async function fetchTrendingAlbums() {
     try {
         const response = await fetch(`${BASE_URL}/api/trending-albums`);
 
-        const data = await response.json();
-
-        if (!response.ok) {
-            console.error('Failed to fetch trending albums:', data.msg);
+        const contentType = response.headers.get("content-type");
+        if (!response.ok || !contentType || !contentType.includes("application/json")) {
+            console.error('Unexpected response format:', await response.text());
             return null;
         }
+
+        const data = await response.json();
 
         return data.data;
     } catch (error) {
