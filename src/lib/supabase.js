56ff1b8b3840dd14c29af60d4793a3c116a37912
Sachin <sachineducational555@gmail.com>
2024-09-26 12:54:17 +0530

    Updated: bug fixes

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+export const supabase = createClient('https://rjabwdhwwfrdjtagbedn.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqYWJ3ZGh3d2ZyZGp0YWdiZWRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUwMTU0NjYsImV4cCI6MjA0MDU5MTQ2Nn0.wC1w_9AxPr1VNqbVs-V7L5ub28u_305VCdHgbvdqsI8');
