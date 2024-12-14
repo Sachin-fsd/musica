6256499ca82f77a130e9a36ddd2c3e86043a7757
Sachin <sachineducational555@gmail.com>
2024-12-14 09:29:38 +0530

    Revert "Updated: improved ui"
    This reverts commit aab8c5d7ed9fa2c16b293088cd931269f1e6b634.

--- src/components/homePage/TopAlbums/AlbumBar/index.js
+++ src/components/homePage/TopAlbums/AlbumBar/index.js
@@ -6,32 +6,30 @@ import { UserContext } from "@/context";
 import { useCallback, useContext, useState } from "react";
 import { debounce } from "lodash";
 import { fetchAlbumSongs } from "@/utils/playAndFetchSuggestionUtils";
-import { Play } from "lucide-react";
+import { Play } from "lucide-react"; // Import the Play icon
 
 const AlbumBar = ({ album }) => {
-    const { currentSong, currentIndex, songList, setSongList, setCurrentIndex, setCurrentSong, setPlaying, setCurrentId } = useContext(UserContext);
+    const { currentSong, currentIndex, songList, setSongList, setCurrentIndex, setCurrentSong, setPlaying, setCurrentId, setLoading, setIsLooping } = useContext(UserContext);
 
-    const [imageError, setImageError] = useState(false);
+    const [imageError, setImageError] = useState(false)
 
     const truncateTitle = (title, maxLength = 24) => {
         return title?.length > maxLength ? `${title?.substring(0, maxLength)}...` : title;
     };
 
-    const handleAlbumClick = useCallback(
-        debounce(() => {
-            const context = { currentSong, currentIndex, songList, setSongList, setCurrentIndex, setCurrentSong, setPlaying, setCurrentId };
-            fetchAlbumSongs(album?.type, album?.id, context);
-        }, 300),
-        [album?.type, album?.id, fetchAlbumSongs, songList]
-    );
+    // Define a debounced function
+    const handleAlbumClick = useCallback(debounce(() => {
+        const context = { currentSong, currentIndex, songList, setSongList, setCurrentIndex, setCurrentSong, setPlaying, setCurrentId }
+        fetchAlbumSongs(album?.type, album?.id, context);
+    }, 300), [album?.type, album?.id, fetchAlbumSongs, songList]);
 
     return (
         <div
-            className="group relative flex flex-col items-center rounded-lg overflow-hidden w-full cursor-pointer transition-transform transform hover:scale-101 sm:hover:underline"
+            className="relative flex flex-col items-center rounded-lg overflow-hidden w-full cursor-pointer transition-transform transform hover:scale-101 sm:hover:underline"
             onClick={handleAlbumClick}
         >
             <div className="relative w-full pb-[100%]">
-                <div className="absolute top-0 left-0 w-full h-full transition-opacity duration-300 sm:group-hover:opacity-80">
+                <div className="absolute  top-0 left-0 w-full h-full transition-opacity duration-300 sm:hover:opacity-80">
                     {album?.image && !imageError ? (
                         <img
                             src={album?.image}
@@ -48,18 +46,15 @@ const AlbumBar = ({ album }) => {
                     )}
                 </div>
                 {/* Play button overlay */}
-                <div className="absolute inset-0 flex items-center justify-center group">
-                    {/* Play Button */}
-                    <div className="relative flex items-center justify-center w-12 h-12 transform scale-0 transition-all duration-300 ease-out opacity-0 group-hover:scale-100 group-hover:opacity-100">
-                        <div className="absolute w-full h-full bg-green-600 rounded-full shadow-lg shadow-green-500/50 hover:bg-green-500"></div>
-                        <Play className="relative w-6 h-6 text-black" />
+                <div className=" absolute w-full h-full flex bottom-0 right-0  duration-300  sm:hover:translate-x-0 translate-x-12 ">
+                    <div className="absolute bottom-0 right-0 -translate-x-1 -translate-y-1 bg-green-600 bg-opacity-100 rounded-full p-3 hover:scale-105 hover:bg-green-500">
+                        <Play className="w-5 h-5 text-black fill-black" />
                     </div>
                 </div>
-
             </div>
             <div className="w-full text-pretty mt-2 px-2 cursor-pointer">
                 {album?.title ? (
-                    <Label className="font-bold cursor-pointer text-gray-800 dark:text-gray-300 text-sm">
+                    <Label className="font-bold cursor-pointer text-gray-800 dark:text-gray-300 text-sm ">
                         {truncateTitle(album.title)}
                     </Label>
                 ) : (
--- src/components/homePage/TopSongs/SongContentCarousel/songBarCarousel.js
+++ src/components/homePage/TopSongs/SongContentCarousel/songBarCarousel.js
@@ -7,12 +7,17 @@ import { useCallback, useContext, useState } from "react";
 import { debounce } from "lodash";
 import { playAndFetchSuggestions } from "@/utils/playAndFetchSuggestionUtils";
 import { Play } from "lucide-react";
-import { decodeHtml } from "@/utils";
+import { decodeHtml, htmlParser } from "@/utils";
 import Marquee from "react-fast-marquee";
 
 const SongBarCarousel = ({ song, index }) => {
     const { currentSong, currentIndex, songList, setSongList, setCurrentIndex, setCurrentSong, setPlaying, setCurrentId, currentId, setLoading } = useContext(UserContext);
     const [imageError, setImageError] = useState(false);
+
+    const truncateTitle = (title, maxLength = 18) => {
+        let result = title?.length > maxLength ? `${title?.substring(0, maxLength)}...` : title;
+        return decodeHtml(result);
+    };
 
     const handlePlayClick = useCallback(
         debounce(async () => {
@@ -32,9 +37,8 @@ const SongBarCarousel = ({ song, index }) => {
     const imageUrl = song?.image[1]?.url; // Verify if the image URL exists
 
     return (
-
         <div
-            className="relative flex flex-col items-center p-1 rounded-lg overflow-hidden w-full cursor-pointer transition-transform transform hover:scale-101 sm:hover:underline"
+            className="relative flex flex-col items-center p-1 rounded-lg overflow-hidden w-full cursor-pointer transition-transform transform hover:scale-101 hover:underline"
             onClick={handlePlayClick}
         >
             <div className="relative w-full pb-[100%]">
@@ -54,11 +58,9 @@ const SongBarCarousel = ({ song, index }) => {
                     )}
                 </div>
                 {/* Play button overlay */}
-                <div className="absolute inset-0 flex items-center justify-center group">
-                    {/* Play Button */}
-                    <div className="relative flex items-center justify-center w-12 h-12 transform scale-0 transition-all duration-300 ease-out opacity-0 sm:group-hover:scale-100 sm:group-hover:opacity-100">
-                        <div className="absolute w-full h-full bg-green-600 rounded-full shadow-lg shadow-green-500/50 hover:bg-green-500"></div>
-                        <Play className="relative w-6 h-6 text-black" />
+                <div className=" absolute w-full h-full flex bottom-0 right-0  duration-300  sm:hover:translate-x-0 translate-x-14 ">
+                    <div className="absolute bottom-0 right-0 -translate-x-1 -translate-y-1 bg-green-600 bg-opacity-100 rounded-full p-3 hover:scale-105 hover:bg-green-500">
+                        <Play className="w-5 h-5 text-black fill-black" />
                     </div>
                 </div>
             </div>
