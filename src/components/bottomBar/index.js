8920a505f82b2aac1f755fc50c5beb0654940b0d
Sachin <sachineducational555@gmail.com>
2025-01-21 18:38:34 +0530

    Updated: BottomBar;reduced memoization

--- src/components/bottomBar/index.js
+++ src/components/bottomBar/index.js
@@ -1,9 +1,8 @@
-'use client'
+'use client';
 import { UserContext } from "@/context";
-import { memo, useMemo } from "react";
+import { memo, useCallback, useContext, useState } from "react";
 import { debounce } from "lodash";
 import { Play, Pause, StepForward, ChevronDown } from "lucide-react";
-import { useContext, useState } from "react";
 import { Skeleton } from "../ui/skeleton";
 import { Label } from "../ui/label";
 import { decodeHtml, htmlParser } from "@/utils";
@@ -16,26 +15,17 @@ const Bottombar = () => {
     const [isSheetOpen, setIsSheetOpen] = useState(false);
     const [imageError, setImageError] = useState(false);
     const { currentSong, playing, currentTime, duration, togglePlayPause, handleNext, handleSeek } = useContext(UserContext);
-    const MemoizedSlider = memo(Slider);
-    const MemoizedRightSidebar = memo(RightSidebar);
-    const MemoizedSheetContent = memo(SheetContent);
-    const MemoizedSheetTrigger = memo(SheetTrigger);
-    const debouncedHandleSeek = useMemo(() => debounce(handleSeek, 300), [handleSeek]);
+
+    const debouncedHandleSeek = useCallback(debounce(handleSeek, 300), [handleSeek]);
 
     if (!currentSong) return null;
 
-    const songImage = useMemo(() => {
-        if (currentSong?.image[0]?.url && !imageError) {
-            return <img src={currentSong?.image[0]?.url} loading="lazy" className="rounded-lg" alt={`${currentSong?.name} cover`} onError={() => setImageError(true)} />;
-        } else {
-            return <Skeleton className="w-12 h-12 rounded-lg bg-gray-700" />;
-        }
-    }, [currentSong, imageError]);
+    const { name, image, artists, album } = currentSong;
 
     return (
         <div className="fixed bottom-0 left-0 w-full bg-gray-900 text-white shadow-lg p-4 pt-0 flex flex-col items-center justify-between flex-grow z-10">
             <div className="w-full pb-1">
-                <MemoizedSlider
+                <Slider
                     onValueChange={debouncedHandleSeek}
                     value={[currentTime]}
                     max={duration}
@@ -45,47 +35,79 @@ const Bottombar = () => {
 
             <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
                 <div className="flex w-full items-center justify-between">
-                    <MemoizedSheetTrigger asChild>
+                    <SheetTrigger asChild>
                         <div className="flex items-center space-x-4 cursor-pointer">
-                            {songImage}
+                            {image?.[0]?.url && !imageError ? (
+                                <img
+                                    src={image[0].url}
+                                    loading="lazy"
+                                    className="rounded-lg"
+                                    alt={`${name} cover`}
+                                    onError={() => setImageError(true)}
+                                />
+                            ) : (
+                                <Skeleton className="w-12 h-12 rounded-lg bg-gray-700" />
+                            )}
                             <div className="flex flex-col overflow-hidden">
-                                {currentSong?.name ? (
-                                    <Label className="font-semibold text-gray-100 truncate text-base cursor-pointer">{decodeHtml(currentSong?.name)}</Label>
+                                {name ? (
+                                    <Label className="font-semibold text-gray-100 truncate text-base cursor-pointer">
+                                        {decodeHtml(name)}
+                                    </Label>
                                 ) : (
                                     <Skeleton className="w-32 h-4 mb-1 bg-gray-700" />
                                 )}
-                                {currentSong?.artists?.primary[0]?.name ? (
+                                {artists?.primary?.[0]?.name && (
                                     <span className="flex whitespace-nowrap">
-                                        <p className="text-sm text-gray-400 truncate">{htmlParser(currentSong?.artists?.primary[0]?.name)} </p> <span className=""> • </span>
-                                        <p className="text-sm text-gray-400 truncate"> {currentSong?.album?.name?.length > 15 ? htmlParser(currentSong?.album?.name?.substring(0, 15).concat("...")) : htmlParser(currentSong?.album?.name)}</p>
+                                        <p className="text-sm text-gray-400 truncate">
+                                            {htmlParser(artists.primary[0].name)}
+                                        </p>
+                                        <span> • </span>
+                                        <p className="text-sm text-gray-400 truncate">
+                                            {album?.name?.length > 15
+                                                ? htmlParser(album.name.substring(0, 15).concat("..."))
+                                                : htmlParser(album?.name)}
+                                        </p>
                                     </span>
-                                ) : null}
+                                )}
                             </div>
                         </div>
-                    </MemoizedSheetTrigger>
+                    </SheetTrigger>
 
                     <div className="flex items-center space-x-4">
-                        <Button variant="simple" className="p-2 bg-pink-500 rounded-full" onClick={togglePlayPause}>
+                        <Button
+                            variant="simple"
+                            className="p-2 bg-pink-500 rounded-full"
+                            onClick={togglePlayPause}
+                            aria-label={playing ? "Pause" : "Play"}
+                        >
                             {playing ? <Pause className="w-6 h-6" /> : <Play className="w-6 h-6" />}
                         </Button>
-                        <Button variant="simple" className="p-2 bg-gray-700 rounded-full" onClick={handleNext}>
+                        <Button
+                            variant="simple"
+                            className="p-2 bg-gray-700 rounded-full"
+                            onClick={handleNext}
+                            aria-label="Next Song"
+                        >
                             <StepForward className="w-6 h-6" />
                         </Button>
                     </div>
                 </div>
 
-                <MemoizedSheetContent side="bottom" className="h-full w-full bg-gradient-to-b bg-fuchsia-300 dark:from-slate-950 dark:to-gray-900 rounded-t-lg p-0">
+                <SheetContent
+                    side="bottom"
+                    className="h-full w-full bg-gradient-to-b bg-fuchsia-300 dark:from-slate-950 dark:to-gray-900 rounded-t-lg p-0"
+                >
                     <div className="flex flex-col h-full">
                         <div className="flex justify-between items-center ml-2 mt-2">
                             <SheetClose asChild>
-                                <Button className="cursor-pointer bg-transparent">
+                                <Button className="cursor-pointer bg-transparent" aria-label="Close">
                                     <ChevronDown className="text-2xl" />
                                 </Button>
                             </SheetClose>
                         </div>
-                        <MemoizedRightSidebar />
+                        <RightSidebar />
                     </div>
-                </MemoizedSheetContent>
+                </SheetContent>
             </Sheet>
         </div>
     );
--- src/components/leftSidebar/installApp/installPrompt.js
+++ src/components/leftSidebar/installApp/installPrompt.js
@@ -37,6 +37,9 @@ const InstallPromptIcon = () => {
   }, []);
 
   const handleDownloadClick = () => {
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+    }
     if (isAndroid) {
       // Download APK
       const apkUrl = "/Musica.apk"; // Replace with your APK URL
@@ -62,7 +65,7 @@ const InstallPromptIcon = () => {
   return (
     <div
       onClick={handleDownloadClick}
-      className="flex items-center w-full p-2 rounded-md hover:bg-slate-200 dark:hover:bg-slate-700 hover:scale-105 transition-transform duration-200 ease-in-out group cursor-pointer"
+      className="flex flex-col items-center w-full p-2 rounded-md hover:bg-slate-200 dark:hover:bg-slate-700 hover:scale-105 transition-transform duration-200 ease-in-out group cursor-pointer"
     >
       <div className="flex items-center justify-center w-10 h-10 rounded-full bg-slate-100 dark:bg-slate-800 text-slate-800 dark:text-slate-200 group-hover:text-slate-900 dark:group-hover:text-slate-300">
         <Download />
