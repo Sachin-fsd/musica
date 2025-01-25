378f8078e112bd9322b8dd0e7dec610c9a9962d4
Sachin <sachineducational555@gmail.com>
2025-01-25 13:55:10 +0530

    Updated: Bottombar bug fixes

--- src/components/bottomBar/index.js
+++ src/components/bottomBar/index.js
@@ -1,41 +1,28 @@
 'use client'
-import { memo, useContext, useMemo, useState } from "react";
-import RightSidebar from "../rightSidebar";
-import { debounce } from "lodash";
-import { Slider } from "./BottomSlider";
-import { Sheet, SheetClose, SheetContent, SheetTrigger } from "../ui/sheet";
 import { UserContext } from "@/context";
-import { Label } from "../ui/label";
-import { Button } from "../ui/button";
+import { Play, Pause, StepForward, ChevronDown } from "lucide-react";
+import { useContext, useState } from "react";
 import { Skeleton } from "../ui/skeleton";
-import { ChevronDown, Pause, Play, StepForward } from "lucide-react";
-import { decodeHtml } from "@/utils";
+import { Label } from "../ui/label";
+import { decodeHtml, htmlParser } from "@/utils";
+import { Button } from "../ui/button";
+import { Sheet, SheetClose, SheetContent, SheetTrigger } from "../ui/sheet";
+import RightSidebar from "../rightSidebar";
+import { Slider } from "./BottomSlider";
 
 const Bottombar = () => {
     const [isSheetOpen, setIsSheetOpen] = useState(false);
-    const [imageError, setImageError] = useState(false);
-    const { currentSong, playing, currentTime, duration, togglePlayPause, handleNext, handleSeek } = useContext(UserContext);
-    const MemoizedSlider = memo(Slider);
-    const MemoizedRightSidebar = memo(RightSidebar);
-    const MemoizedSheetContent = memo(SheetContent);
-    const MemoizedSheetTrigger = memo(SheetTrigger);
-    const debouncedHandleSeek = useMemo(() => debounce(handleSeek, 300), [handleSeek]);
+    const [imageError, setImageError] = useState(false)
+
+    const { togglePlayPause, currentSong, playing, handleSeek, currentTime, duration, handleNext } = useContext(UserContext);
 
     if (!currentSong) return null;
-
-    const songImage = useMemo(() => {
-        if (currentSong?.image[0]?.url && !imageError) {
-            return <img src={currentSong?.image[0]?.url} loading="lazy" className="rounded-lg" alt={`${currentSong?.name} cover`} onError={() => setImageError(true)} />;
-        } else {
-            return <Skeleton className="w-12 h-12 rounded-lg bg-gray-700" />;
-        }
-    }, [currentSong, imageError]);
 
     return (
         <div className="fixed bottom-0 left-0 w-full bg-gray-900 text-white shadow-lg p-4 pt-0 flex flex-col items-center justify-between flex-grow z-10">
             <div className="w-full pb-1">
-                <MemoizedSlider
-                    onValueChange={debouncedHandleSeek}
+                <Slider
+                    onValueChange={handleSeek}
                     value={[currentTime]}
                     max={duration}
                     className="bg-gray-200 dark:bg-gray-800 rounded-full"
@@ -43,10 +30,20 @@ const Bottombar = () => {
             </div>
 
             <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
+                {/* Song Image and Info */}
                 <div className="flex w-full items-center justify-between">
-                    <MemoizedSheetTrigger asChild>
+                    <SheetTrigger asChild>
                         <div className="flex items-center space-x-4 cursor-pointer">
-                            {songImage}
+                            {currentSong?.image[0]?.url && !imageError ? (
+                                <img
+                                    src={currentSong?.image[0]?.url}
+                                    loading="lazy"
+                                    className="rounded-lg"
+                                    alt={`${currentSong?.name} cover`}
+                                    onError={() => setImageError(true)} />
+                            ) : (
+                                <Skeleton className="w-12 h-12 rounded-lg bg-gray-700" />
+                            )}
                             <div className="flex flex-col overflow-hidden">
                                 {currentSong?.name ? (
                                     <Label className="font-semibold text-gray-100 truncate text-base cursor-pointer">{decodeHtml(currentSong?.name)}</Label>
@@ -55,39 +52,45 @@ const Bottombar = () => {
                                 )}
                                 {currentSong?.artists?.primary[0]?.name ? (
                                     <span className="flex whitespace-nowrap">
-                                        <p className="text-sm text-gray-400 truncate">{decodeHtml(currentSong?.artists?.primary[0]?.name)} </p> <span className=""> • </span>
-                                        <p className="text-sm text-gray-400 truncate"> {decodeHtml(currentSong?.album?.name)}</p>
+                                        <p className="text-sm text-gray-400 truncate">{htmlParser(currentSong?.artists?.primary[0]?.name)} </p> <span className=""> • </span>
+                                        <p className="text-sm text-gray-400 truncate"> {currentSong?.album?.name?.length > 15 ? htmlParser(currentSong?.album?.name?.substring(0, 15).concat("...")) : htmlParser(currentSong?.album?.name)}</p>
                                     </span>
                                 ) : null}
                             </div>
                         </div>
-                    </MemoizedSheetTrigger>
+                    </SheetTrigger>
 
                     <div className="flex items-center space-x-4">
-                        <Button variant="simple" className="p-2 bg-pink-500 rounded-full" onClick={togglePlayPause}>
-                            {playing ? <Pause className="w-6 h-6" /> : <Play className="w-6 h-6" />}
+                        <Button variant="simple" className="p-2 bg-pink-500 rounded-full text-white shadow-md hover:bg-pink-600 hover:text-accent-foreground" onClick={togglePlayPause}>
+                            {playing ? (
+                                <Pause className="w-6 h-6" />
+                            ) : (
+                                <Play className="w-6 h-6" />
+                            )}
                         </Button>
-                        <Button variant="simple" className="p-2 bg-gray-700 rounded-full" onClick={handleNext}>
+                        <Button variant="simple" className="p-2 bg-gray-700 rounded-full text-white shadow-md hover:bg-gray-800 hover:text-accent-foreground" onClick={handleNext}>
                             <StepForward className="w-6 h-6" />
                         </Button>
                     </div>
                 </div>
-
-                <MemoizedSheetContent side="bottom" className="h-full w-full bg-gradient-to-b bg-fuchsia-300 dark:from-slate-950 dark:to-gray-900 rounded-t-lg p-0">
+                <SheetContent side="bottom" className="h-full w-full bg-gradient-to-b bg-fuchsia-300 dark:from-slate-950 dark:to-gray-900 rounded-t-lg p-0">
                     <div className="flex flex-col h-full">
                         <div className="flex justify-between items-center ml-2 mt-2">
                             <SheetClose asChild>
-                                <Button className="cursor-pointer bg-transparent">
-                                    <ChevronDown className="text-2xl" />
+                                <Button className="cursor-pointer bg-transparent hover:bg-gray-200 dark:hover:bg-gray-700 sm:hover:shadow-md transition-all duration-200">
+                                    <ChevronDown className="text-2xl font-bold text-gray-900 dark:text-gray-100" />
                                 </Button>
                             </SheetClose>
+
                         </div>
-                        <MemoizedRightSidebar />
+                        <div className="flex-1 overflow-hidden">
+                            <RightSidebar />
+                        </div>
                     </div>
-                </MemoizedSheetContent>
+                </SheetContent>
             </Sheet>
         </div>
     );
 };
 
-export default memo(Bottombar);
+export default Bottombar;
