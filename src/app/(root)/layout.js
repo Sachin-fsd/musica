28f61e89e9c68763199a64880064c4827f5ae3e9
Sachin Singh <122505800+Sachin-fsd@users.noreply.github.com>
2025-04-02 22:43:40 +0530

    updated: layout

--- src/app/(root)/layout.js
+++ src/app/(root)/layout.js
@@ -1,77 +1,39 @@
 import LeftSidebar from "@/components/leftSidebar";
-
 import RightSidebar from "@/components/rightSidebar";
-
 import Navbar from "@/components/navbar";
-
 import UserState from "@/context";
-
 import Bottombar from "@/components/bottomBar";
 
-
-
 export default function RootLayout({ children }) {
+  return (
+    <UserState>
+      <div className="grid h-screen w-screen grid-rows-[auto,1fr] grid-cols-[auto,1fr] md:grid-cols-[auto,1fr,auto] sm:max-w-full">
 
-  return (
+        {/* LeftSidebar shown for medium and large screens */}
+        <div className="hidden md:block md:row-span-2">
+          <LeftSidebar />
+        </div>
 
-    <UserState>
+        {/* Navbar spanning the top row */}
+        <div className="col-span-2 md:col-span-2">
+          <Navbar />
+        </div>
 
-      <div className="grid h-screen w-screen grid-rows-[auto,1fr] grid-cols-[auto,1fr] md:grid-cols-[auto,1fr,auto] sm:max-w-full">
+        {/* Main content area */}
+        <div className="overflow-y-auto overflow-x-hidden col-span-2 md:col-span-1">
+          {children}
+        </div>
 
+        {/* RightSidebar only for large screens */}
+        <div className="hidden lg:block lg:row-span-2 w-full min-w-[20%] max-w-[95%]">
+          <RightSidebar />
+        </div>
 
-
-        {/* LeftSidebar shown for medium and large screens */}
-
-        <div className="hidden md:block md:row-span-2">
-
-          <LeftSidebar />
-
-        </div>
-
-
-
-        {/* Navbar spanning the top row */}
-
-        <div className="col-span-2 md:col-span-2">
-
-          <Navbar />
-
-        </div>
-
-
-
-        {/* Main content area */}
-
-        <div className="overflow-y-auto overflow-x-hidden col-span-2 md:col-span-1">
-
-          {children}
-
-        </div>
-
-
-
-        {/* RightSidebar only for large screens */}
-
-        <div className="hidden lg:block lg:row-span-2 w-full min-w-[20%] max-w-[95%]">
-
-          <RightSidebar />
-
-        </div>
-
-
-
-        {/* Bottom Player for small and medium screens */}
-
-        <div className="md:fixed md:bottom-0 w-full col-span-2 lg:hidden">
-
-          <Bottombar />
-
-        </div>
-
-      </div>
-
-    </UserState>
-
-  );
-
+        {/* Bottom Player for small and medium screens */}
+        <div className="md:fixed md:bottom-0 w-full col-span-2 lg:hidden">
+          <Bottombar />
+        </div>
+      </div>
+    </UserState>
+  );
 }
