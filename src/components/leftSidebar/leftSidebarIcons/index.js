0a708026742b3fafa96068f1736863458ce824e8
Sachin-fsd <sachineducational555@gmail.com>
2025-12-31 20:38:26 +0530

    fix:left navbar highlight issue

--- src/components/leftSidebar/leftSidebarIcons/index.js
+++ src/components/leftSidebar/leftSidebarIcons/index.js
@@ -1,10 +1,14 @@
+'use client'
+
 import { leftIcons } from "@/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import AdjustSongQuality from "../AdjustSongQuality";
 import InstallPromptIcon from "../installApp/installPrompt";
 import JamOnOff from "../jamOnOff";
 
 const LeftSidebarIcons = ({ setIsSheetOpen }) => {
+    const pathname = usePathname();
     return (
         <div className="flex flex-col items-center w-full">
             <div className="flex flex-col  items-centre space-y-3">
@@ -13,13 +17,19 @@ const LeftSidebarIcons = ({ setIsSheetOpen }) => {
                         onClick={setIsSheetOpen ? ()=>setIsSheetOpen(false) : null}
                         key={icon.label}
                         href={icon.link}
-                        className="flex md:flex-col items-center w-full p-2 rounded-md hover:bg-slate-200 dark:hover:bg-slate-700 hover:scale-105 transition-transform duration-200 ease-in-out group"
+                        className={`flex md:flex-col items-center w-full p-2 rounded-md hover:bg-slate-200 dark:hover:bg-slate-700 hover:scale-105 transition-transform duration-200 ease-in-out group ${
+                            pathname === icon.link ? 'bg-slate-300 dark:bg-slate-600' : ''
+                        }`}
                     >
-                        <div className="flex items-center justify-center w-10 h-10 rounded-full bg-slate-100 dark:bg-slate-800 text-slate-800 dark:text-slate-200 group-hover:text-slate-900 dark:group-hover:text-slate-300">
+                        <div className={`flex items-center justify-center w-10 h-10 rounded-full bg-slate-100 dark:bg-slate-800 text-slate-800 dark:text-slate-200 group-hover:text-slate-900 dark:group-hover:text-slate-300 ${
+                            pathname === icon.link ? 'text-slate-900 dark:text-slate-100' : ''
+                        }`}>
                             {icon.image}
                         </div>
                         <span
-                            className="md:text-xs text-slate-800 dark:text-slate-200 group-hover:text-slate-900 dark:group-hover:text-slate-300  font-bold"
+                            className={`md:text-xs text-slate-800 dark:text-slate-200 group-hover:text-slate-900 dark:group-hover:text-slate-300 font-bold ${
+                                pathname === icon.link ? 'text-slate-900 dark:text-slate-100' : ''
+                            }`}
                         >
                             {icon.label}
                         </span>
