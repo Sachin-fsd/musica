384ab98db73251071383423b6a4e57beb862ffa6
Sachin-fsd <sachineducational555@gmail.com>
2026-03-14 23:17:37 +0530

    fix:repeated albums loading bug

--- src/components/homePage/TopAlbums/AlbumContent/index.js
+++ src/components/homePage/TopAlbums/AlbumContent/index.js
@@ -1,13 +1,8 @@
 'use client'
-// import { All_Albums, mega_menu_3 } from "@/utils/cachedSongs";
-import { All_Albums } from "@/utils/cachedSongs";
 import TopAlbums from "..";
-// import { fetchByLinkAction } from "@/app/actions"; // Remove server action import
-import { shuffleArray } from "@/utils/extraFunctions";
 import { useEffect, useState } from 'react';
 
 const AlbumContent = () => {
-    // const backendUrl = 'http://localhost:5000';
     const [albumData, setAlbumData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
@@ -16,28 +11,35 @@ const AlbumContent = () => {
         const fetchAllData = async () => {
             try {
                 setLoading(true);
-                const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/jiosaavn-data`);
-                if (!response.ok) {
-                    throw new Error(`HTTP error! status: ${response.status}`);
+                const StoredAlbumData = JSON.parse(localStorage.getItem("albumsFetched"));
+                if (StoredAlbumData.time + (12 * 60 * 60 * 1000) < new Date) {
+                    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/jiosaavn-data`);
+                    if (!response.ok) {
+                        throw new Error(`HTTP error! status: ${response.status}`);
+                    }
+                    const MegaMenu3Playlists = await response.json();
+                    const object_for_megaMenu3 = [
+                        { heading: "Charts", data: MegaMenu3Playlists.charts },
+                        { heading: "Ultimate Jams", data: MegaMenu3Playlists.top_playlists },
+                        { heading: "New Trending", data: MegaMenu3Playlists.new_trending },
+                        { heading: "Artist Recos", data: MegaMenu3Playlists["artist_recos"] },
+                        { heading: "New Albums", data: MegaMenu3Playlists.new_albums },
+                        { heading: "Top Hits", data: MegaMenu3Playlists["promo:vx:data:68"] },
+                        { heading: "On Repeat", data: MegaMenu3Playlists["promo:vx:data:185"] },
+                        { heading: "Chill Vibes", data: MegaMenu3Playlists["promo:vx:data:113"] },
+                        { heading: "Vibe Check", data: MegaMenu3Playlists["promo:vx:data:116"] },
+                        { heading: "Fresh Finds", data: MegaMenu3Playlists["promo:vx:data:143"] },
+                        { heading: "Epic Soundtracks", data: MegaMenu3Playlists["promo:vx:data:76"] },
+                        { heading: "Chill Mood", data: MegaMenu3Playlists["promo:vx:data:21"] },
+                        { heading: "Liked By All", data: MegaMenu3Playlists["promo:vx:data:31"] },
+                    ];
+                    const StoredAlbumData = { albumsArray: object_for_megaMenu3, time: new Date };
+                    setAlbumData(object_for_megaMenu3);
+                    localStorage.setItem("albumsFetched", JSON.parse(StoredAlbumData))
                 }
-                const MegaMenu3Playlists = await response.json();
-                console.log("Data from backend:", MegaMenu3Playlists);
-                const object_for_megaMenu3 = [
-                    { heading: "Charts", data: MegaMenu3Playlists.charts },
-                    { heading: "Ultimate Jams", data: MegaMenu3Playlists.top_playlists },
-                    { heading: "New Trending", data: MegaMenu3Playlists.new_trending },
-                    { heading: "New Albums", data: MegaMenu3Playlists.new_albums },
-                    { heading: "Top Hits", data: MegaMenu3Playlists["promo:vx:data:68"] },
-                    { heading: "On Repeat", data: MegaMenu3Playlists["promo:vx:data:185"] },
-                    { heading: "Chill Vibes", data: MegaMenu3Playlists["promo:vx:data:113"] },
-                    { heading: "Vibe Check", data: MegaMenu3Playlists["promo:vx:data:116"] },
-                    { heading: "Fresh Finds", data: MegaMenu3Playlists["promo:vx:data:143"] },
-                    { heading: "Epic Soundtracks", data: MegaMenu3Playlists["promo:vx:data:76"] },
-                    { heading: "Chill Mood", data: MegaMenu3Playlists["promo:vx:data:21"] },
-                    { heading: "Liked By All", data: MegaMenu3Playlists["promo:vx:data:31"] },
-                    { heading: "Artist Recos", data: MegaMenu3Playlists["artist_recos"] },
-                ];
-                setAlbumData(object_for_megaMenu3);
+                else {
+                    setAlbumData(StoredAlbumData.albumsArray);
+                }
                 setLoading(false);
             } catch (err) {
                 console.error("Error fetching data from backend:", err);
@@ -45,12 +47,11 @@ const AlbumContent = () => {
                 setLoading(false);
             }
         };
-
         fetchAllData();
     }, []); // Empty dependency array to run only once on mount
 
     if (loading) {
-        return <div className="p-4 bg-gray-100 dark:bg-gray-900 rounded-lg text-center">Loading albums...</div>;
+        return <div className="mt-4 p-4 bg-gray-100 dark:bg-gray-900 rounded-lg text-center">Loading albums...</div>;
     }
 
     if (error) {
